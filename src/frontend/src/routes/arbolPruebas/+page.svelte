dc7f3a49a3fe7defc22d759305d07d2d4a229af3
unknown <ivanneo711@gmail.com>
2026-06-09 19:02:45 +0200

    First functional graph

---
+++ src/frontend/src/routes/arbolPruebas/+page.svelte
@@ -0,0 +1,39 @@
+<script>
+import { SvelteFlow,Background, MiniMap, Controls, Panel } from '@xyflow/svelte';
+import '@xyflow/svelte/dist/style.css';
+
+let nodes=$state.raw([{id:'1', position:{x:0,y:0},data:{label:'B'}},
+    {id:'2', position:{x:100,y:100},data:{label:'A'}}
+]);
+
+let edges=$state.raw([
+{id:'e1-2', source:'1',target:'2'}
+
+]);
+
+
+</script>
+
+
+<div id="Main">
+
+    <h1>Pruebas</h1>
+
+    <div style:width='100vw' style:height='100vh'>
+        <SvelteFlow bind:nodes bind:edges fitView>
+            <Background/>
+            
+            <Controls/>
+            <Panel position='top-left'>
+                <div>
+                    <p>By Iván</p>
+                </div>
+            </Panel>
+
+            <Panel position='bottom-right'>
+                <MiniMap/>
+            </Panel>
+        </SvelteFlow>
+    </div>
+
+</div>
