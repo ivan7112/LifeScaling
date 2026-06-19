5d544707af2e5a54241bc06f028cee5051eeecb4
unknown <ivanneo711@gmail.com>
2026-06-19 18:02:24 +0200

    API REST beta

--- src/backend/api/apiBackend.js
+++ src/backend/api/apiBackend.js
@@ -23,14 +23,57 @@ async function loadApiArboles(app){
     const db=client.db('GraphDB');
     const col=db.collection('trees');
 
-
+    //GET
     app.get('/graphs',async (req,res)=>{
         const data=await col.find().toArray();
-        return res.send(data);
+        return res.status(200).send(data);
 
     });
 
-    
+    app.get('/graphs/:id',async (req,res)=>{
+        const data=col.find({_id:req.params.id});
+        return res.status(200).send(data);
+    });
+
+    //POST
+    app.post('/graphs',async (req,res)=>{
+        if(!(req.body.nodes && req.body.edges && req.body.name)) res.sendStatus(400);
+        //Verificar sintáxis nodos-edges? Replicados?
+
+        col.insertOne({nodes:`${req.body.nodes}`,edges:`${req.body.edges}`,name:`${req.body.name}`});
+        res.sendStatus(201);
+    });
+
+    app.post('graphs/:id',async (req,res)=>{
+        res.sendStatus(405);
+    })
+
+    //PUT
+
+    app.put('/graphs/:id', async (req,res)=>{
+
+        if(!(req.body.nodes && req.body.edges && req.body.name)) res.sendStatus(400);
+
+        col.updateOne({_id:`${req.params.id}`},
+            {$set:{nodes:`${req.body.nodes}`,edges:`${req.body.edges}`,name:`${req.body.name}`}});
+        res.sendStatus(200);
+    });
+
+    app.put('/graphs', async (req,res)=>{
+        res.sendStatus(405);
+    });
+
+
+    //DELETE
+
+    app.delete('/graphs',async (req,res)=>{
+        col.deleteMany({});
+        res.sendStatus(200);
+    });
+
+    app.delete('graphs/:id', async (req,res)=>{
+        col.deleteOne({_id:req.params.id});
+    });
 
 
 }
